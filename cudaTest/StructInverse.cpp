92d2c4bcf7b88ca4a37f69be9481c19097b2e510
Chao <936156784@qq.com>
2021-03-11 14:39:02 +0800

    2021/3/11

--- cudaTest/StructInverse.cpp
+++ cudaTest/StructInverse.cpp
@@ -36,58 +36,66 @@ int main()
 {
 	const int N(3), n(3);
 
-	float src_h[n * n * N];
 	
-	init_array(src_h, N, n);
+	float src_h[n * n * N];			//cpu数组src_h；
+	init_array(src_h, N, n);		//cpu数组src_h初始化；
 
-	float* src_d = NULL;
-	checkCudaErrors(cudaMalloc((void**)&src_d, n * n * N * sizeof(float)));
-	checkCudaErrors(cudaMemcpy(src_d,src_h, n * n * N * sizeof(float),cudaMemcpyHostToDevice));
-	float* A[N];
-	set_array_device(A, src_d, N, n);
+	float* src_d = NULL;			
+	checkCudaErrors(cudaMalloc((void**)&src_d, n * n * N * sizeof(float)));						//gpu数组src_d分配内存；
+	checkCudaErrors(cudaMemcpy(src_d,src_h, n * n * N * sizeof(float),cudaMemcpyHostToDevice));	//cpu数组src_h复制到gpu端src_d；
+	float* A[N];						//cpu上声明存放gpu数组src_d指针的数组；
+	set_array_device(A, src_d, N, n);	//数组A寻址；
 
-	float** A_d = NULL;
-	//checkCudaErrors(cudaMalloc<float*>(&A_d, sizeof(A)));
-	checkCudaErrors(cudaMalloc((void**)&A_d, sizeof(A)));
-	checkCudaErrors(cudaMemcpy(A_d, A, sizeof(A), cudaMemcpyHostToDevice));
+	float** A_d = NULL;				
+	checkCudaErrors(cudaMalloc((void**)&A_d, sizeof(A)));						//gpu上声明存放gpu数组src_d指针的数组；
+	checkCudaErrors(cudaMemcpy(A_d, A, sizeof(A), cudaMemcpyHostToDevice));		//cpu端复制A至gpu端；
 
-	//checkCudaErrors(cudaMemcpy(A, A_d, sizeof(A), cudaMemcpyDeviceToHost));
 
-	float dst_h[n * n * N];
-	init_array(dst_h, N, n);
+	float dst_h[n * n * N];			//cpu数组dst_h；
+	init_array(dst_h, N, n);		//cpu数组dst_h初始化；
 
-	float* C[N];
-	float* dst_d = NULL;
-	checkCudaErrors(cudaMalloc((void**)&dst_d, n * n * N * sizeof(float)));
-	set_array_device(C, dst_d, N, n);
+	float* C[N];					//cpu上声明存放gpu数组dst_d指针的数组；
+	float* dst_d = NULL;			
+	checkCudaErrors(cudaMalloc((void**)&dst_d, n * n * N * sizeof(float)));	//gpu数组dst_d分配内存；
+	set_array_device(C, dst_d, N, n);										//数组C寻址；
 	
 	float** C_d = NULL;
-	checkCudaErrors(cudaMalloc((void**)&C_d, sizeof(C)));
-	//checkCudaErrors(cudaMalloc<float*>(&C_d, sizeof(C)));
-	checkCudaErrors(cudaMemcpy(C_d, C, sizeof(C), cudaMemcpyHostToDevice));
+	checkCudaErrors(cudaMalloc((void**)&C_d, sizeof(C)));					//gpu上声明存放gpu数组dst_d指针的数组；
+	checkCudaErrors(cudaMemcpy(C_d, C, sizeof(C), cudaMemcpyHostToDevice));	////cpu端复制A至gpu端；
 
-	cublasHandle_t handle;
-	cublasCreate_v2(&handle);
-	int batchSize = N;
-	int* P, * INFO;
+	cublasHandle_t handle;			
+	cublasCreate_v2(&handle);					//创建句柄；
+	int batchSize = N;							//矩阵块个数；
+	int* P = NULL;								//用于记录LU分解的信息；
+	int* INFO = NULL;							//用于记录LU分解是否成功；
 	checkCudaErrors(cudaMalloc((void**)&P, n * batchSize * sizeof(int)));
 	checkCudaErrors(cudaMalloc((void**)&INFO, batchSize * sizeof(int)));
-	checkCudaErrors(cublasSgetrfBatched(handle, n, A_d, n, P, INFO, batchSize));
+
+	checkCudaErrors(cublasSgetrfBatched(handle, n, A_d, n, P, INFO, batchSize));	//LU分解;参数lda为主导维cuBLAS为列主导，即决定了行数；
 
 	int INFOh = 0;
 	checkCudaErrors(cudaMemcpy(&INFOh, INFO, sizeof(int), cudaMemcpyDeviceToHost));
 
-	if (INFOh == n)
+	if (INFOh == n)//检查矩阵是否奇异；
 	{
 		fprintf(stderr, "Factorization Failed: Matrix is singular\n");
 		cudaDeviceReset();
 		exit(EXIT_FAILURE);
 	}
 
-	checkCudaErrors(cublasSgetriBatched(handle, n, A_d, n, P, C_d, n, INFO, batchSize));
+	checkCudaErrors(cublasSgetriBatched(handle, n, A_d, n, P, C_d, n, INFO, batchSize));//将LU分解的结果转换为逆矩阵；其中C_d保存的是dst_d指针的数组，即二级指针；
+
+	checkCudaErrors(cudaMemcpy(&INFOh, INFO, sizeof(int), cudaMemcpyDeviceToHost));
+	if (INFOh != 0)//检查矩阵是否奇异；
+	{
+		fprintf(stderr, "Inversion Failed: Matrix is singular\n");
+		cudaDeviceReset();
+		exit(EXIT_FAILURE);
+	}
 
 	checkCudaErrors(cudaMemcpy(dst_h, dst_d, n*n*N*sizeof(float), cudaMemcpyDeviceToHost));
-	//cudaMemcpy(dst_h, dst_d, n * n * N * sizeof(float), cudaMemcpyDeviceToHost);
+
+	//释放内存及销毁句柄；
 	cudaFree(C_d);
 	cudaFree(A_d);
 	cudaFree(P), cudaFree(INFO), cublasDestroy_v2(handle);
